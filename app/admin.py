a550d8e5510a74bf66bb761c6827223aa3bb1f7f
felgonzalezj <82742452+felgonzalezj@users.noreply.github.com>
2022-04-27 13:05:53 -0400

    27-04-2022

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tipo_Usuario , Account, Profesional, Publicacion, Paciente, Fonoaudiologo_paciente, Paciente_Profesional
+from .models import Tipo_Usuario , Account, Profesional, Publicacion, Paciente, Fonoaudiologo_paciente, Paciente_Profesional, Enfermeria_paciente, Medicamento, MedicamentoCompra
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
@@ -17,7 +17,7 @@ class Tipo_usuarioAdmin(admin.ModelAdmin):
     list_display = ["id", "nombre_tipo_usuario", "codigo_tipo_usuario", "descripcion_tipo_usuario"]
 
 class ProfesionalAdmin(admin.ModelAdmin):
-    list_display = ["id", "id_app_usuario", "id_tipo_usuario", "rutprofesional", "nombreprofesional","apellidopaternoprofesional", "id_tipo_usuario","email","telefono","whatsapp", "telegram"]
+    list_display = ["id", "id_app_usuario", "id_tipo_usuario", "rutprofesional", "nombreprofesional","apellidopaternoprofesional", "email","telefono","whatsapp", "telegram"]
 
 class PublicacionAdmin(admin.ModelAdmin):
     list_display = ["id_publicacion", "titulo", "fecha_publicacion", "categoria", "autor", "texto", "imagen"]
@@ -31,6 +31,15 @@ class PacienteAdmin(admin.ModelAdmin):
 class Paciente_ProfesionalAdmin(admin.ModelAdmin):
     list_display = ["idapp_paciente","idapp_profesional", "id_tipo_usuario"]
 
+class Enfermeria_pacienteAdmin(admin.ModelAdmin):
+    list_display = ["id", "id_app_usuario","idapp_paciente","fecha_contactabilidad","bitacora_contactabilidad"]
+
+class MedicamentoAdmin(admin.ModelAdmin):
+    list_display = ["id", "nombrelab","nombremedicamen","dosismedicamento","cantidaddisplay"]
+
+class MedicamentoCompraAdmin(admin.ModelAdmin):
+    list_display = ["id_app_usuario","idapp_paciente" ,"idapp_medicamento", "fecha_compra"]
+
 admin.site.unregister(User)
 admin.site.register(User, CustomizedUserAdmin)
 admin.site.register(Tipo_Usuario, Tipo_usuarioAdmin)
@@ -39,3 +48,6 @@ admin.site.register(Publicacion, PublicacionAdmin)
 admin.site.register(Paciente, PacienteAdmin)
 admin.site.register(Fonoaudiologo_paciente, FonoPacienteAdmin)
 admin.site.register(Paciente_Profesional, Paciente_ProfesionalAdmin)
+admin.site.register(Enfermeria_paciente, Enfermeria_pacienteAdmin)
+admin.site.register(Medicamento, MedicamentoAdmin)
+admin.site.register(MedicamentoCompra, MedicamentoCompraAdmin)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 import email
+from statistics import mode
 from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import User
@@ -119,3 +120,44 @@ class Paciente_Profesional(models.Model):
 
     def __int__(self):
         return self.id
+
+
+class Enfermeria_paciente(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_app_usuario = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Id Usuario")
+    idapp_paciente = models.ForeignKey(Paciente, on_delete=models.PROTECT, verbose_name="Rut Paciente")
+    fecha_contactabilidad = models.DateField()
+    bitacora_contactabilidad = models.TextField()
+    class Meta:
+        verbose_name = "Enfermeria - Paciente"
+        verbose_name_plural = "Enfermeria - Paciente"
+
+    def __int__(self):
+        return self.id
+
+
+class Medicamento(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombrelab = models.CharField(max_length=30, verbose_name="Nombre Laboratorio")
+    nombremedicamen = models.CharField(max_length=30, verbose_name="Nombre Medicamento")
+    dosismedicamento = models.CharField(max_length=40, verbose_name="Dosis Medicamento")
+    cantidaddisplay = models.CharField(max_length=30, verbose_name="Cantidad Medicamento Display")
+    class Meta:
+        verbose_name = "Medicamento"
+        verbose_name_plural = "Medicamentos"
+
+    def __str__(self):
+        return self.nombremedicamen + ' / ' + self.nombrelab + ' / ' + self.dosismedicamento + ' / ' + self.cantidaddisplay
+
+
+class MedicamentoCompra(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_app_usuario = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Id Usuario")
+    idapp_paciente = models.ForeignKey(Paciente, on_delete=models.PROTECT, verbose_name="Rut Paciente")
+    idapp_medicamento = models.ForeignKey(Medicamento, on_delete=models.PROTECT, verbose_name="Medicamento/Laboratorio/Dosis/Cant.Display")
+    fecha_compra = models.DateField()
+
+    def __int__(self):
+        return self.id
+
+    
